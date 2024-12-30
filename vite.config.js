8f259a701da208cd4ec43af01a7fdbb2e3666bc9
DaniRox <danirox.disenioydesarrollo@gmail.com>
2024-12-30 09:43:59 -0300

    proyecto impulzze

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import ghPages from 'vite-plugin-gh-pages';
-import svgr from 'vite-plugin-svgr';
-
 
 export default defineConfig({
-  plugins: [
-    react(),
-    svgr(),
-  ],
-  base: "/impulzze/",
+  plugins: [react()],
+  base: '/impulzze/',
 });
