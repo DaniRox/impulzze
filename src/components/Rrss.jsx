94c245ee9ed93e9f31a1eed1f96a879675f15b95
DaniRox <danirox.disenioydesarrollo@gmail.com>
2025-02-14 19:20:05 -0300

    sitio terminado

--- src/components/Rrss.jsx
+++ src/components/Rrss.jsx
@@ -4,20 +4,15 @@ const Rrss = () => {
     return (
         <div className={styles.Rrss__content}>
 
-            <button className={styles.Rrss__icon}>
-                <a href="">
-                    <img src="/images/phone.png" alt="" className={styles.Rrss_imgIcon}/>
-                </a>
-            </button>
 
             <button className={styles.Rrss__icon}>
-                <a href="">
+                <a href="https://www.instagram.com/impulzze?igsh=MXBzajhhMmc0OGd3Ng%3D%3D&utm_source=qr" target="_blank">
                     <img src="/images/ig.png" alt="" className={styles.Rrss_imgIcon}/>
                 </a>
             </button>
 
             <button className={styles.Rrss__icon}>
-                <a href="">
+                <a href="https://www.linkedin.com/company/impulzze/about/" target="_blank">
                     <img src="/images/in.png" alt="" className={styles.Rrss_imgIcon}/>
                 </a>
             </button>
